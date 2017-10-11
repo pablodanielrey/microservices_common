1e540adbe50ebbcddf2c400f78ad4487fa0e3c2b
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-10-11 14:19:59 -0300

    cambie el numero de version

--- docker/src/setup.py
+++ docker/src/setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages
 
 setup(name='microservices_common',
-          version='0.1.0.dev2',
+          version='0.1.0.dev3',
           description='Proyecto que implementa las librerías comunes para los microservicios',
           url='https://github.com/pablodanielrey/microservices_common',
           author='Desarrollo DiTeSi, FCE',
