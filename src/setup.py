b4f669f7788ac1d3a096395cc9f0944ecff1eaca
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-12-17 11:06:14 -0300

    cambie la version

--- src/setup.py
+++ src/setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages
 
 setup(name='microservices_common',
-          version='2.0.5a3',
+          version='2.0.5a4',
           description='Proyecto que implementa las librerías comunes para los microservicios',
           url='https://github.com/pablodanielrey/microservices_common',
           author='Desarrollo DiTeSi, FCE',
