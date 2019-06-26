11702a05873e3a3669f6fea37b7dfc917bca20a8
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-06-26 14:57:30 -0300

    agrego los converters aca

--- src/setup.py
+++ src/setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages
 
 setup(name='microservices_common',
-          version='2.0.7a1',
+          version='2.0.8a1',
           description='Proyecto que implementa las librerías comunes para los microservicios',
           url='https://github.com/pablodanielrey/microservices_common',
           author='Desarrollo DiTeSi, FCE',
